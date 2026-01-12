3c08c8244f7954f99d0450bf81b2681ba6c5d95c
lirazshaka <liraz@mypart.com>
2026-01-12 09:11:34 +0200

    Add textured stone background image

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import BottomNav from './BottomNav'
 
 export default function Layout() {
   return (
-    <div className="min-h-screen bg-canvas">
+    <div className="min-h-screen">
       <Header />
       <div className="lg:pl-20">
         <main className="px-6 py-6 pb-24 max-w-[600px] mx-auto lg:max-w-[1200px] lg:pt-24 lg:pb-12">
